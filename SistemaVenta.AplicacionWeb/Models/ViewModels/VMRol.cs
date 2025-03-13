a696b417133c8e37dce6f32778fd14ed8aef5a0a
Brandon De La Cruz <brandon.delacruz@coppel.com>
2025-03-13 11:05:19 -0700

    Creacion de los ViewModels e implementación de AutoMapper.

---
+++ SistemaVenta.AplicacionWeb/Models/ViewModels/VMRol.cs
@@ -0,0 +1,13 @@
+﻿namespace SistemaVenta.AplicacionWeb.Models.ViewModels
+{
+    public class VMRol
+    {
+        public partial class Rol
+        {
+            public int IdRol { get; set; }
+
+            public string? Descripcion { get; set; }
+        }
+
+    }
+}
